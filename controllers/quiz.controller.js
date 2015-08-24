d2fbf3747fa31ea4337ab927838a06e14eff18de
Jannet24 <letty_217@hotmail.com>
2015-08-24 12:34:01 -0500

    Autoload

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -1,18 +1,36 @@
 var models = require('../models/models.js');
 
-exports.question = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question',{pregunta:quiz[0].pregunta})
+exports.load = function(req,res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			}
+			else
+			{
+				next(new Error('No existe quizId= '+ quizId));
+			}
+		}
+		).catch(function(error){next(error);});
+};
+exports.show = function(req,res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz: req.quiz});
 	})
 };
 
 exports.answer = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta ==='Roma') {
-		res.render('quizes/answer',{respuesta:'Correcto'});
+		var resultado = 'Incorrecto';
+		if (req.query.respuesta === req.quiz.respuesta) {
+			resultado = 'Correcto';
 		}
-		else{
-		res.render('quizes/answer',{respuesta:'Incorrecto'});
-		}
+		res.render('quizes/answer',
+			{ quiz: req.quiz, respuesta: resultado});
+};
+
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs',{quizes: quizes});
 	})
 };
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ var router = express.Router();
 var quizController = require('../controllers/quiz.controller');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes',		        quizController.index);	
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',   quizController.answer);
 module.exports = router;
